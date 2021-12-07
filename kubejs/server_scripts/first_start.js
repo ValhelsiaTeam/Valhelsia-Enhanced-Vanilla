7daba9761a0bb400e167497160fb203cb75d85cb
Khytwel <54046029+Khytwel@users.noreply.github.com>
2021-12-07 21:53:22 +0100

    Update first_start.js

--- kubejs/server_scripts/first_start.js
+++ kubejs/server_scripts/first_start.js
@@ -2,6 +2,6 @@ onEvent('player.logged_in', function (event) {
   if (!event.player.stages.has('first_start')) {
     event.player.stages.add('first_start')
     
-    event.server.runCommandSilent(`/playerstats ${event.player.name} points 5`)
+    event.server.runCommandSilent(`/playerstats ${event.player.name} points 1`)
   }
 })
