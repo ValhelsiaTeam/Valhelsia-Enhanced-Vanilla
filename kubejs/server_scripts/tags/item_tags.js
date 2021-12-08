1779ac626aa38c07107317decf41f57da664ebf2
Khytwel <54046029+Khytwel@users.noreply.github.com>
2021-12-08 11:31:08 +0100

    Update item_tags.js

--- kubejs/server_scripts/tags/item_tags.js
+++ kubejs/server_scripts/tags/item_tags.js
@@ -2,6 +2,32 @@
 onEvent('tags.items', event => {
   
   //
+  event.get('fabric:swords').add([
+    'ae2:certus_quartz_sword',
+    'ae2:nether_quartz_sword',
+  ])
+
+  event.get('fabric:pickaxes').add([
+    'ae2:certus_quartz_pickaxe',
+    'ae2:nether_quartz_pickaxe',
+  ])
+
+  event.get('fabric:axes').add([
+    'ae2:certus_quartz_axe',
+    'ae2:nether_quartz_axe',
+  ])
+
+  event.get('fabric:shovels').add([
+    'ae2:certus_quartz_shovel',
+    'ae2:nether_quartz_shovel',
+  ])
+
+  event.get('fabric:hoes').add([
+    'ae2:certus_quartz_hoe',
+    'ae2:nether_quartz_hoe',
+  ])
+
+  //
   event.get('harvest_scythes:machetes').add([
     'harvest_scythes:wooden_machete',
     'harvest_scythes:stone_machete',
