7101fd3a6b05786ae6787efa606f47a6db4c8a9d
Khytwel <54046029+Khytwel@users.noreply.github.com>
2021-12-20 16:09:37 +0100

    Remove bread recipe from Vanilla (duplicate recipe).

--- kubejs/server_scripts/recipes/remove.js
+++ kubejs/server_scripts/recipes/remove.js
@@ -4,6 +4,9 @@ onEvent('recipes', function (event) {
 
   var idRemove = [
 
+    // Minecraft
+    'minecraft:bread', // Duplicate recipe.
+
     // Oxidized
     'oxidized:copper_nugget', // Duplicate recipe.
     'oxidized:rose_gold_sword', // Duplicate recipe.
