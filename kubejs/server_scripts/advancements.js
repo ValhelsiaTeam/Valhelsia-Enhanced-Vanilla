e45e1b6e6b40004a16a58346a8c45c11deeecc20
Vaelzan <vaelzan@evilcult.net>
2022-01-12 01:17:25 +1100

    Add blank advancement script (unused currently).

---
+++ kubejs/server_scripts/advancements.js
@@ -0,0 +1,5 @@
+onEvent('player.advancement', function (event) {
+  // Note: Currently disabled until (if) LevelZ adds a way to add points instead of just setting total points.
+  //event.player.tell(event.advancement.id())
+  //event.server.runCommandSilent(`/playerstats ${event.player.name} points 1`)
+})
