83992aaa3f690deb7317bc546e14c6b45db409db
Vaelzan <vaelzan@evilcult.net>
2022-02-19 15:03:03 +1100

    Fix Tech Reborn's raw ore storage blocks -> raw ore recipes, too.

--- kubejs/server_scripts/recipes/replace.js
+++ kubejs/server_scripts/recipes/replace.js
@@ -7,6 +7,13 @@ onEvent('recipes', function (event) {
   event.replaceOutput({id: 'techreborn:crafting_table/storage_block/raw_silver_storage_block'}, 'techreborn:silver_storage_block', 'techreborn:raw_silver_storage_block')
   event.replaceOutput({id: 'techreborn:crafting_table/storage_block/raw_tin_storage_block'}, 'techreborn:tin_storage_block', 'techreborn:raw_tin_storage_block')
   event.replaceOutput({id: 'techreborn:crafting_table/storage_block/raw_tungsten_storage_block'}, 'techreborn:tungsten_storage_block', 'techreborn:raw_tungsten_storage_block')
+  
+  event.replaceInput({id: 'techreborn:crafting_table/raw/raw_iridium_from_block'}, 'techreborn:iridium_storage_block', 'techreborn:raw_iridium_storage_block')
+  event.replaceInput({id: 'techreborn:crafting_table/raw/raw_lead_from_block'}, 'techreborn:lead_storage_block', 'techreborn:raw_lead_storage_block')
+  event.replaceInput({id: 'techreborn:crafting_table/raw/raw_silver_from_block'}, 'techreborn:silver_storage_block', 'techreborn:raw_silver_storage_block')
+  event.replaceInput({id: 'techreborn:crafting_table/raw/raw_tin_from_block'}, 'techreborn:tin_storage_block', 'techreborn:raw_tin_storage_block')
+  event.replaceInput({id: 'techreborn:crafting_table/raw/raw_tungsten_from_block'}, 'techreborn:tungsten_storage_block', 'techreborn:raw_tungsten_storage_block')
+
 
   // Inter-mod compatibility:
   event.replaceInput({}, 'betterend:ender_dust', '#c:ender_pearl_dusts')
