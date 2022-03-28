fbbe6b7a0ff2f64bd0e916026b991f094bf226a2
Vaelzan <vaelzan@evilcult.net>
2022-03-29 05:32:37 +1100

    Add recipes to create Sky Stone via the Implosion Compressor.

--- kubejs/server_scripts/recipes/techreborn.js
+++ kubejs/server_scripts/recipes/techreborn.js
@@ -16,9 +16,24 @@ onEvent('recipes', (event) => {
       results: [
         Item.of(output).toJson()
       ]
-    })
+    });
+  }
+
+  // Implosion Compressor Recipe Template
+  const implosion = (outputs, inputs, power, time) => {
+    event.custom({
+      type: "techreborn:implosion_compressor",
+      power: power,
+      time: time,
+      ingredients: inputs,
+      results: outputs
+    });
   }
 
   // ----- Grinder Recipes -----
-  grind('techreborn:marble_dust', 'blockus:marble', 2, 1440)
+  grind('techreborn:marble_dust', 'blockus:marble', 2, 1440);
+
+  // ----- Implosion Compressor Recipes -----
+  implosion(['8x ae2:sky_stone_block', '16x techreborn:dark_ashes_dust'], ['8x minecraft:crying_obsidian', '16x minecraft:tnt'], 30, 2000);
+  implosion(['8x ae2:sky_stone_block', '4x techreborn:ender_eye_small_dust'], ['8x minecraft:crying_obsidian', '4x minecraft:end_crystal'], 30, 2000);
 });
