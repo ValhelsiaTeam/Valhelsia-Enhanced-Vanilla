7d98e46c519b3b210f44a4f8c6c3e5cd019db170
Vaelzan <vaelzan@evilcult.net>
2022-03-29 06:16:51 +1100

    Add AE2 presses to End City loot (since Meteorites are broken).

---
+++ kubejs/server_scripts/loot_tables/chests.js
@@ -0,0 +1,19 @@
+// priority: 10
+// Valhelsia: Enhanced Vanilla
+// Additional Chest Loot
+
+onEvent('chest.loot_tables', event => {
+
+  // End City Treasure Chest Injection
+  event.modify('minecraft:end_city_treasure', table => {
+    table.addPool(pool => {
+      // One random press per end city chest, guaranteed.
+      pool.rolls = 1
+      pool.addItem('ae2:calculation_processor_press');
+      pool.addItem('ae2:engineering_processor_press');
+      pool.addItem('ae2:logic_processor_press');
+      pool.addItem('ae2:silicon_press');
+    });
+  });
+
+});
