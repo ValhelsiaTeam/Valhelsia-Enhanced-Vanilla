9892bac8c2357832e29ee91184b781ff6fc3bf90
Vaelzan <vaelzan@evilcult.net>
2022-08-19 20:01:12 +1000

    Add color constants to KubeJS for future use.

---
+++ kubejs/server_scripts/constants.js
@@ -0,0 +1,31 @@
+// priority: 9001
+// Valhelsia: Enhanced Vanilla
+
+/**
+ * @file Global Constants for use in other scripts.
+ */
+
+/**
+ * Minecraft's default colors, as used in vanilla item IDs and by most mods.
+ * Note: Some mods use abnormal names for colors, such as Absent By Design (which uses 'silver' in some places 
+ * instead of 'light_gray'), so this shouldn't be used blindly without verifying that the mod uses standard
+ * color names.
+ */
+ const COLORS = [
+  'white',
+  'orange',
+  'magenta',
+  'light_blue',
+  'yellow',
+  'lime',
+  'pink',
+  'gray',
+  'light_gray',
+  'cyan',
+  'purple',
+  'blue',
+  'brown',
+  'green',
+  'red',
+  'black'
+];
