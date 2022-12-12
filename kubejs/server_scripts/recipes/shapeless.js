fd0666ddcc52bb8e760385047c6f85f251aac6a9
Khytwel <khytwel@gmail.com>
2022-12-12 16:49:02 +0100

    Update shapeless.js

--- kubejs/server_scripts/recipes/shapeless.js
+++ kubejs/server_scripts/recipes/shapeless.js
@@ -9,14 +9,7 @@
 /**
  * Shapeless Recipe Event Handler
  */
-onEvent('recipes', event => {
-  
-  // Architect's Palette
-  event.shapeless('architects_palette:heavy_mossy_stone_bricks', ['architects_palette:heavy_stone_bricks', 'immersive_weathering:moss_clump']);
-  event.shapeless('architects_palette:heavy_mossy_stone_bricks', ['architects_palette:heavy_stone_bricks', 'minecraft:moss_block']);
-
-  // Blockus
-  event.shapeless('blockus:mossy_deepslate_bricks', ['minecraft:deepslate_bricks', 'immersive_weathering:moss_clump']);
+ServerEvents.recipes(event => {
 
   // Twigs
   event.shapeless('twigs:mossy_cobblestone_bricks', ['twigs:cobblestone_bricks', 'immersive_weathering:moss_clump']);
