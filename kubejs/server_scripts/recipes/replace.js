ccbe0ea9bc8506777465ec832f69b1209316a3b0
Khytwel <khytwel@gmail.com>
2023-01-09 14:12:06 +0100

    Update replace.js

--- kubejs/server_scripts/recipes/replace.js
+++ kubejs/server_scripts/recipes/replace.js
@@ -8,12 +8,13 @@
  * The majority of the recipe changes in this script are to convert recipe inputs to
  * use tags instead of specific items, or adjusting the output where we have a prefered mod to use.
  * 
+ * @copyright Valhelsia Inc 2023
  */
 
 /**
  * Recipe Replacement Event Handler
  */
-onEvent('recipes', (event) => {
+ServerEvents.recipes(event => {
 
   // Convenience Functions
   const replaceInputID = (recipeID, from, to) => { event.replaceInput({id: recipeID}, from, to, true)};
@@ -21,30 +22,8 @@ onEvent('recipes', (event) => {
   const replaceInput = (from, to) => { event.replaceInput({}, from, to, true)};
   const replaceOutput = (from, to) => { event.replaceOutput({}, from, to, true)};
 
-  // ----- Bug Fixes -----
-  replaceOutputID('techreborn:crafting_table/storage_block/raw_iridium_storage_block', 'techreborn:iridium_storage_block', 'techreborn:raw_iridium_storage_block');
-  replaceOutputID('techreborn:crafting_table/storage_block/raw_lead_storage_block', 'techreborn:lead_storage_block', 'techreborn:raw_lead_storage_block');
-  replaceOutputID('techreborn:crafting_table/storage_block/raw_silver_storage_block', 'techreborn:silver_storage_block', 'techreborn:raw_silver_storage_block');
-  replaceOutputID('techreborn:crafting_table/storage_block/raw_tin_storage_block', 'techreborn:tin_storage_block', 'techreborn:raw_tin_storage_block');
-  replaceOutputID('techreborn:crafting_table/storage_block/raw_tungsten_storage_block', 'techreborn:tungsten_storage_block', 'techreborn:raw_tungsten_storage_block');
-  
-  replaceInputID('techreborn:crafting_table/raw/raw_iridium_from_block', 'techreborn:iridium_storage_block', 'techreborn:raw_iridium_storage_block');
-  replaceInputID('techreborn:crafting_table/raw/raw_lead_from_block', 'techreborn:lead_storage_block', 'techreborn:raw_lead_storage_block');
-  replaceInputID('techreborn:crafting_table/raw/raw_silver_from_block', 'techreborn:silver_storage_block', 'techreborn:raw_silver_storage_block');
-  replaceInputID('techreborn:crafting_table/raw/raw_tin_from_block', 'techreborn:tin_storage_block', 'techreborn:raw_tin_storage_block');
-  replaceInputID('techreborn:crafting_table/raw/raw_tungsten_from_block', 'techreborn:tungsten_storage_block', 'techreborn:raw_tungsten_storage_block');
-
   // ----- Inter-mod Compatibility -----
-  replaceInput('betterend:ender_dust', '#c:ender_pearl_dusts');
-  replaceInput('techreborn:ender_pearl_dust', '#c:ender_pearl_dusts');
   replaceInput('ae2:ender_dust', '#c:ender_pearl_dusts');
   replaceInput('farmersdelight:tree_bark', '#immersive_weathering:bark');
 
-  // ----- Unification -----
-  replaceInput('blockus:snow_bricks', 'ecologics:snow_bricks');
-  replaceInput('blockus:amethyst_bricks', 'twigs:polished_amethyst_bricks');
-  replaceInput('blockus:polished_amethyst', 'twigs:polished_amethyst');
-
-  replaceOutput('betterend:ender_dust', 'techreborn:ender_pearl_dust');
-  replaceOutput('ae2:ender_dust', 'techreborn:ender_pearl_dust');
 });
