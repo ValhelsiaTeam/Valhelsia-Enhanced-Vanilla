767ee9677f100efeef71c020bc1e398e797e512b
Khytwel <khytwel@gmail.com>
2023-02-23 14:01:35 +0100

    Create block_tags.js

---
+++ kubejs/server_scripts/tags/block_tags.js
@@ -0,0 +1,19 @@
+// priority: 80
+// Valhelsia: Enhanced Vanilla
+
+/**
+ * @file Block Tag Additions for Valhelsia: Enhanced Vanilla.
+ * 
+ * These are mostly for recipe input unification, but sometimes have other uses.
+ * 
+ * @copyright Valhelsia Inc 2023
+ */
+
+/**
+ * Block Tag Event Handler (to be used for tag addition only).
+ */
+ServerEvents.tags('block', event => {
+
+  // ----- Inter-mod Compatibility -----
+
+});
