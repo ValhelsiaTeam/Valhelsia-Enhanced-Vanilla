928e2d38c65c2ebee354901e04a33f3242ec084d
Vaelzan <vaelzan@evilcult.net>
2023-02-27 09:38:01 +1100

    Initial work on porting config script to MC 1.19.

--- kubejs/server_scripts/config.js
+++ kubejs/server_scripts/config.js
@@ -9,16 +9,19 @@
  * will be created or updated. See the examples for how to use the config in
  * KubeJS scripts.
  * 
+ * Note: There is a single config file per instance, not per world. Changing settings
+ * will impact all saves / worlds. Use NBT instead for per-world configuration.
+ * 
  * It is possible to adjust the config in-game using chat 
- * (via the !vconfig command by default).
+ * (via the /vconfig command by default).
  * 
- * @example Accessing Config Entries:
+ * @example <caption>Accessing Config Entries (via script)</caption>
  * let debug = global.config.debug;
- * let starting_points = global.config.starting_points
+ * let debug = global.config.debug;
  * 
- * @example Setting Config Entries:
- * setConfig('debug', true)
- * setConfig('starting_points', 3)
+ * @example <caption>Setting Config Entries (via script)</caption>
+ * setConfig('debug', true);
+ * setConfig('starting_points', 3);
  * 
  * @copyright Valhelsia Inc 2023
  */
@@ -26,16 +29,25 @@
 /**
  * The filename of the config file.
  * @const {string}
- * @default 
+ * @default  'vev2_config.json
  */
-const CONFIG_FILENAME = 'vev_config.json';
+const CONFIG_FILENAME = 'vev2_config.json';
 
 /**
  * The chat command to modify the config.
  * @const {string}
- * @default
+ * @default 'vconfig'
  */
 const CONFIG_COMMAND = '!vconfig';
+
+/**
+ * The permission level required to use the config command.
+ * @see {@link https://minecraft.fandom.com/wiki/Permission_level#Java_Edition Permission Level}
+ * for information about each permission level.
+ * @const {!number}
+ * @default 3
+ */
+ const CONFIG_COMMAND_PERMISSION_LEVEL = 3;
 
 /**
  * The default values for config options in the pack.
@@ -43,6 +55,7 @@ const CONFIG_COMMAND = '!vconfig';
  */
 const DEFAULT_CONFIG = {
   debug: false,
+  expert: false,
   write_default_reward_data: false,
   log_added_recipes: false,
   log_removed_recipes: false,
@@ -86,38 +99,41 @@ function setConfig(key, value) {
 }
 
 /**
- * Chat event handler that parses config commands.
+ * Command registry handler that adds the config command.
  * 
- * Command Syntax (in-game chat, assuming default command): 
- * Get Config: !vconfig [option]
- * Set Config: !vconfig [option] [setting]
- * 
- * @example Using the config command:
- * Note: This example uses the default value for the command.
- * Announce the current value of the debug entry to chat: !config debug
- * Set the value of the debug entry to true: !config debug true
+ * Command Syntax (assuming default command): 
+ * Get Config: /vconfig [key]
+ * Set Config: /vconfig [key] [value]
  */
 ServerEvents.commandRegistry(event => {
-  if (event.message.startsWith(CONFIG_COMMAND)) {
-    if (event.player.isOp()) {
-      const params = event.message.trim().split(' ')
-      if (params.length < 2) {
-        event.player.tell(Text.translate('valhelsia.config.syntax'));
-      } else if (params.length == 2) {
-        event.player.tell(Text.translate('valhelsia.config.current', `${params[1]}: '${global.config[params[1]]}'`));
-      } else {
-        setConfig(params[1], params[2])
-        event.player.tell(Text.translate('valhelsia.config.updated', `${params[1]}: '${global.config[params[1]]}'`));
-      }
-    } else {
-      event.player.tell(Text.translate('valhelsia.config.no_permission'));
-    }
-    event.cancel();
-  }
+  const { commands: Commands, arguments: Arguments } = event;
+  event.register(
+    Commands.literal(CONFIG_COMMAND)
+      .requires((source) => source.hasPermission(CONFIG_COMMAND_PERMISSION_LEVEL))
+      .then(Commands.argument('key', Arguments.STRING.create(event))
+        .then(Commands.argument('value', Arguments.STRING.create(event))
+          .executes((ctx) => {
+            // Set updated config entry (key, value).
+            const key = Arguments.STRING.getResult(ctx, 'key').toLowerCase();
+            const value = Arguments.STRING.getResult(ctx, 'value');
+            setConfig(key, value);
+            ctx.source.sendSuccess(Text.translate('valhelsia.config.updated', `${key}: '${global.config[key]}'.`), true);
+            return 1;
+          })
+        )
+        .executes((ctx) => {
+          // Get current config entry (key).
+          const key = Arguments.STRING.getResult(ctx, 'key');
+          ctx.source.sendSuccess(Text.translate('valhelsia.config.current', `${key}: '${global.config[key]}'.`), false);
+          return 1;
+        })
+      )
+  );
 });
 
-// Read in current config + set any missing properties to defaults:
+/** @type {?Object} */
 let config = JsonIO.read(CONFIG_FILENAME);
+/** @type {!boolean} */
 let configDirty = false;
 
 if (!config) {
@@ -138,9 +154,3 @@ if (configDirty) {
 }
 
 global.config = config;
-
-// KubeJS Settings
-//settings.logAddedRecipes = config.log_added_recipes ? true : false;
-//settings.logRemovedRecipes = config.log_removed_recipes ? true : false;
-//settings.logSkippedRecipes = config.log_skipped_recipes ? true : false;
-//settings.logErroringRecipes = config.log_erroring_recipes ? true : false;
