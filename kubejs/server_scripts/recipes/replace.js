97fcbfe61380b85e4262e5edd00e9bce0a77f354
Khytwel <khytwel@gmail.com>
2023-03-29 11:21:14 +0200

    Update replace.js

--- kubejs/server_scripts/recipes/replace.js
+++ kubejs/server_scripts/recipes/replace.js
@@ -16,11 +16,18 @@
  */
 ServerEvents.recipes(event => {
 
-  // Convenience Functions
-  const replaceInputID = (recipeID, from, to) => { event.replaceInput({id: recipeID}, from, to, true)};
-  const replaceOutputID = (recipeID, from, to) => { event.replaceOutput({id: recipeID}, from, to, true)};
-  const replaceInput = (from, to) => { event.replaceInput({}, from, to, true)};
-  const replaceOutput = (from, to) => { event.replaceOutput({}, from, to, true)};
+  // ----- Convenience Functions -----
+  // Replace inputs and outputs across all recipes that use the given ingredient / item.
+  const replaceInput = (from, to) => {event.replaceInput({}, from, to)};
+  const replaceOutput = (from, to) => {event.replaceOutput({}, from, to)};
+
+  // Replace inputs / outputs by recipe ID.
+  const replaceInputID = (recipeID, from, to) => {event.replaceInput({id: recipeID}, from, to)};
+  const replaceOutputID = (recipeID, from, to) => {event.replaceOutput({id: recipeID}, from, to)};
+
+  // Replace inputs / outputs by recipe type.
+  const replaceInputType = (recipeType, from, to) => {event.replaceInput({type: recipeType}, from, to)};
+  const replaceOutputType = (recipeType, from, to) => {event.replaceOutput({type: recipeType}, from, to)};
 
   // ----- Inter-mod Compatibility -----
   replaceInput('farmersdelight:tree_bark', '#immersive_weathering:bark');
